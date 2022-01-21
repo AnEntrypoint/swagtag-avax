db562b9688a5eb8a613c8026ee1ef6288cbda1a3
lanmower <almagestfraternite@gmail.com>
2022-01-21 21:14:00 +0000

    testnet restored

--- Testnet-jan21/scripts/swagtag.js
+++
@@ -1,6 +0,0 @@
-var fs = require("fs");
-
-fs.copyFile("build/contracts/swagtag.json", "../src/contracts/swagtag.json", (err) => {
-  if (err) throw err;
-  console.log("✅ Your contract's ABI was copied to the frontend");
-});
