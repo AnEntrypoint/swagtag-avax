34eb20aa7539dff070151db2554714906203ee64
lanmower <almagestfraternite@gmail.com>
2022-01-26 19:07:52 +0000

    Updates

--- setup.sh
+++ setup.sh
@@ -5,6 +5,6 @@ echo 'sudo docker run -d --name nginx-proxy -p 80:80 -p 443:443 -v certs:/etc/ng
 
 echo 'sudo docker run -d --name nginx-proxy-acme --volumes-from nginx-proxy --volume /var/run/docker.sock:/var/run/docker.sock:ro --volume acme:/etc/acme.sh --env "DEFAULT_EMAIL=$1" --restart unless-stopped nginxproxy/acme-companion' >> startacme.sh
 
-echo 'docker run --detach  --name $1  --env "VIRTUAL_HOST=$2" -e "VIRTUAL_PORT=80" -e "LETSENCRYPT_HOST=$2" /home/ubuntu:/usr/share/nginx/html:ro --restart unless-stopped nginx' > startnginx.sh
+echo 'docker run --detach  --name $1  --env "VIRTUAL_HOST=$2" -e "VIRTUAL_PORT=80" -e "LETSENCRYPT_HOST=$2" -v /home/ubuntu:/usr/share/nginx/html:ro --restart unless-stopped nginx ' > startnginx.sh
 
 echo 'sudo docker run -d --name $1 -e "VIRTUAL_HOST=$2" -e "VIRTUAL_PORT=8443" -e "LETSENCRYPT_HOST=$2" -e PUID=1000 -e PGID=1000 -e PASSWORD=$2 -e SUDO_PASSWORD=123123123 -v /home/ubuntu:/config/workspace --restart unless-stopped ghcr.io/linuxserver/code-server' > startcode.sh
